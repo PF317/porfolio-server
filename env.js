0727f30e3d2ff3c78f457ee5d3e11636d9285bdc
unknown <p.francois17@gmail.com>
2020-07-30 16:25:42 -0400

    portfolio sever

---
+++ env.js
@@ -0,0 +1,9 @@
+const info = {
+    user: 'postgres',
+    host:'portfolio-instance.c3lror2gqkcu.us-west-2.rds.amazonaws.com',
+    database: 'projects',
+    password: 'password',
+    port: '5432',
+}
+
+module.exports = info; 
