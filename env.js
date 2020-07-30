92bc971d1b8fd453ca451760da8f51ce8c424900
Patrick Francois <64545271+PF317@users.noreply.github.com>
2020-07-30 16:32:08 -0400

    Delete env.js

--- env.js
+++
@@ -1,9 +0,0 @@
-const info = {
-    user: 'postgres',
-    host:'portfolio-instance.c3lror2gqkcu.us-west-2.rds.amazonaws.com',
-    database: 'projects',
-    password: 'password',
-    port: '5432',
-}
-
-module.exports = info; 
